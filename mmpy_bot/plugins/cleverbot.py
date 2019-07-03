606a6765f7c28644c8f1a192a7803c0422979ad5
dragonballzeke <zeke@egherman.com>
2019-07-02 21:57:05 -0500

    Fix PEP8 issue

---
+++ mmpy_bot/plugins/cleverbot.py
@@ -0,0 +1,22 @@
+import os
+import re
+import requests
+import json
+from mmpy_bot.bot import listen_to, respond_to
+
+
+states = {} #initialize empty dict for states
+API_KEY = os.getenv('CLEVERBOT_API_KEY')
+
+
+@listen_to('^john (.+)$', re.IGNORECASE)  # only reply to messages in public channels that start with "john"
+@respond_to('^(.+)$')  # also reply to all messages that ping john or are in DM
+def cleverbot(message,input):
+    global states
+    state_id = str(message.get_user_id()) + str(message.get_channel_name())  # each user/channel combo has unique state
+    if state_id not in states:
+        states[state_id] = None
+    r = requests.get(url='http://www.cleverbot.com/getreply?key={}&input={}&cs={}'.format(API_KEY,input,states[state_id]), verify=False)
+    msg_out = json.loads(r.text)['output']
+    states[state_id] = json.loads(r.text)['cs']
+    message.send(msg_out)
