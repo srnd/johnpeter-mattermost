f9ee6dc653d603cf7252463da45ef43449480ae7
dragonballzeke <zeke@egherman.com>
2019-11-25 19:19:26 -0600

    make this work as intended (Didn't I commit this earlier???)

--- mmpy_bot/plugins/code_day.py
+++ mmpy_bot/plugins/code_day.py
@@ -2,6 +2,6 @@ import re
 from mmpy_bot.bot import listen_to
 
 
-@listen_to('^.* code day .*$', re.IGNORECASE)
+@listen_to('^.*code day.*$', re.IGNORECASE)
 def code_day(message):
     message.send('BRANDING')
