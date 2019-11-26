54175b2d6ac5c7d968100a363b455f6a1f7d0cb8
dragonballzeke <zeke@egherman.com>
2019-11-25 19:18:26 -0600

    Clumsy-ish way to make her ignore things quieter

--- mmpy_bot/utils.py
+++ mmpy_bot/utils.py
@@ -61,7 +61,7 @@ def allowed_users(*allowed_users_list):
     return plugin
 
 
-def allowed_channels(*allowed_channels_list):
+def allowed_channels(*allowed_channels_list, silent=False):
     def plugin(func):
         @wraps(func)
         def wrapper(message, *args, **kw):
@@ -69,6 +69,8 @@ def allowed_channels(*allowed_channels_list):
             disp_name = message.get_channel_display_name()
             url_name = message.get_channel_name()
             is_direct_message = message.is_direct_message()
+            if silent:
+                return
             if (not is_direct_message) and (not {disp_name, url_name} & set(allowed_channels_list)):
                 return message.reply(
                     "`This plugin only allowed in these channels:{}`"
